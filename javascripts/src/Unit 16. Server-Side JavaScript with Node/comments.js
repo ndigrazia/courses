fcf3695c2b0adf887f5bebccbddb8b537ea23d26
Nelson Di Grazia <nelson.digrazia@telefonica.com>
2023-07-26 12:21:56 -0300

    twenty-first comments

--- javascripts/src/Unit 16. Server-Side JavaScript with Node/comments.js
+++ javascripts/src/Unit 16. Server-Side JavaScript with Node/comments.js
@@ -1616,6 +1616,43 @@ because they interact with the file permissions set by fs.chmod().)
 Finally, you can set the access time and modification time of a file or directory with
 fs.utimes() and fs.futimes() and their variants.**/
 
+
+/*
+    File Mode Strings
+
+    We saw the fs.open() and fs.openSync() methods before when using the low-level
+    API to read files. In that use case, it was sufficient to just pass the filename to the open
+    function. When you want to write a file, however, you must also specify a second
+    string argument that specifies how you intend to use the file descriptor. Some of the
+    available flag strings are as follows:
+
+    "w"
+    Open the file for writing
+    "w+"
+    Open for writing and reading
+    "wx"
+    Open for creating a new file; fails if the named file already exists
+    "wx+"
+    Open for creation, and also allow reading; fails if the named file already exists
+    "a"
+    Open the file for appending; existing content won’t be overwritten
+    "a+"
+    Open for appending, but also allow reading
+
+    If you do not pass one of these flag strings to fs.open() or fs.openSync(), they use
+    the default “r” flag, making the file descriptor read-only. Note that it can also be useful
+    to pass these flags to other file-writing methods:
+    */
+    
+    // Write to a file in one call, but append to anything that is already there.
+    // This works like fs.appendFileSync()
+    fs.writeFileSync("messages.log", "hello", { flag: "a" });
+    
+    // Open a write stream, but throw an error if the file already exists.
+    // We don't want to accidentally overwrite something!
+    // Note that the option above is "flag" and is "flags" here
+    fs.createWriteStream("messages.log", { flags: "wx" });
+
 //16.7.6 Working with Directories
 
 /*
@@ -2057,4 +2094,203 @@ knock-knock joke server) that use SSL-encrypted connections like HTTPS servers d
 
 //16.10 Working with Child Processes
 
+/*
+Node also works well for writing scripts that execute other programs. In Node the “child_process” 
+module defines a number of functions for running other programs as child processes. 
+
+16.10.1 execSync() and execFileSync()
+
+The easiest way to run another program is with child_process.execSync(). This
+function takes the command to run as its first argument. It creates a child process,
+runs a shell in that process, and uses the shell to execute the command you passed.
+Then it blocks until the command (and the shell) exit. If the command exits with an
+error, then execSync() throws an exception. Otherwise, execSync() returns whatever
+output the command writes to its stdout stream. By default this return value is a
+buffer, but you can specify an encoding in an optional second argument to get a
+string instead. If the command writes any output to stderr, that output just gets
+passed through to the parent process’s stderr stream.
+
+So, for example, if you are writing a script and performance is not a concern, you
+might use child_process.execSync() to list a directory with a familiar Unix shell
+command rather than using the fs.readdirSync() function:*/
+
+const child_process = require("child_process");
+let listing = child_process.execSync("ls -l web/*.html", {encoding: "utf8"});
+
+/*
+If you don’t need the features of a shell, you can avoid the overhead of starting a shell
+by using child_process.execFileSync(). 
+
+This function executes a program directly,
+without invoking a shell. But since no shell is involved, it can’t parse a command line,
+and you must pass the executable as the first argument and an array of command-line
+arguments as the second argument:*/
+
+let listing2 = child_process.execFileSync("ls", ["-l", "web/"], {encoding: "utf8"});
+
+/*
+
+    Child Process Options
+
+    execSync() and many of the other child_process functions have a second or third
+    optional argument that specifies additional details about how the child process is to
+    run. The encoding property of this object was used earlier to specify that we’d like the
+    command output to be delivered as a string rather than as a buffer. Other important
+    properties that you can specify include the following:
+
+    • cwd specifies the working directory for the child process. If you omit this, then
+    the child process inherits the value of process.cwd().
+    • env specifies the environment variables that the child process will have access to.
+    • input specifies a string or buffer of input data that should be used as the standard
+    input to the child process. This option is only available to the synchronous functions
+    that do not return a ChildProcess object.
+    • maxBuffer specifies the maximum number of bytes of output that will be collected
+    by the exec functions. If a child process produces more output than this, it will be killed and
+    will exit with an error.
+    • shell specifies the path to a shell executable or true. For child process functions
+    that normally execute a shell command, this option allows you to specify which
+    shell to use.
+    • timeout specifies the maximum number of milliseconds that the child process
+    should be allowed to run.If it has not exited before this time elapses, it will be
+    killed and will exit with an error
+    • uid specifies the user ID (a number) under which the program should be run.
+
+*/
+
+//16.10.2 exec() and execFile()
+
+/*
+The execSync() and execFileSync() functions are, as their names indicate, synchronous:
+they block and do not return until the child process exits.  Using these
+functions is a lot like typing Unix commands in a terminal window: they allow you to
+run a sequence of commands one at a time. But if you’re writing a program that needs
+to accomplish a number of tasks, and those tasks don’t depend on each other in any
+way, then you may want to parallelize them and run multiple commands at the same
+time. You can do this with the asynchronous functions child_process.exec() and
+child_process.execFile().
+
+exec() and execFile() are like their synchronous variants except that they return
+immediately with a ChildProcess object that represents the running child process,
+and they take an error-first callback as their final argument. The callback is invoked
+when the child process exits, and it is actually called with three arguments. The first is
+the error, if any; it will be null if the process terminated normally. The second argument
+is the collected output that was sent to the child’s standard output stream. And
+the third argument is any output that was sent to the child’s standard error stream.
+
+The ChildProcess object returned by exec() and execFile() allows you to terminate
+the child process, and to write data to it (which it can then read from its standard
+input).
+
+If you plan to execute multiple child processes at the same time, then it may be easiest
+to use the “promisified” version of exec() which returns a Promise object which, if
+the child process exits without error, resolves to an object with stdout and stderr
+properties. Here, for example, is a function that takes an array of shell commands as
+its input and returns a Promise that resolves to the result of all of those commands:*/
+
+const child_process = require("child_process");
+const util = require("util");
+const execP = util.promisify(child_process.exec);
+
+function parallelExec(commands) {
+    // Use the array of commands to create an array of Promises
+    let promises = commands.map(command => execP(command, {encoding: "utf8"}));
+    // Return a Promise that will fulfill to an array of the fulfillment
+    // values of each of the individual promises. (Instead of returning objects
+    // with stdout and stderr properties we just return the stdout value.)
+    return Promise.all(promises).then(outputs => outputs.map(out => out.stdout));
+}
+
+module.exports = parallelExec;
+
+
+//16.10.3 spawn()
+
+/*
+The various exec functions described so far—both synchronous and asynchronous—
+are designed to be used with child processes that run quickly and do not produce a
+lot of output. Even the asynchronous exec() and execFile() are nonstreaming: they
+return the process output in a single batch, only after the process has exited.
+
+The child_process.spawn() function allows you streaming access to the output of
+the child process, while the process is still running. It also allows you to write data to
+the child process (which will see that data as input on its standard input stream): this
+means it is possible to dynamically interact with a child process, sending it input
+based on the output it generates.
+
+spawn() does not use a shell by default, so you must invoke it like execFile() with
+the executable to be run and a separate array of command-line arguments to pass to
+it. spawn() returns a ChildProcess object like execFile() does, but it does not take a
+callback argument. Instead of using a callback function, you listen to events on the
+ChildProcess object and on its streams.
+
+The ChildProcess object returned by spawn() is an event emitter. You can listen for
+the “exit” event to be notified when the child process exits. A ChildProcess object also
+has three stream properties. stdout and stderr are Readable streams: when the child
+process writes to its stdout and its stderr streams, that output becomes readable
+through the ChildProcess streams. Note the inversion of the names here. In the child
+process, “stdout” is a Writable output stream, but in the parent process, the stdout
+property of a ChildProcess object is a Readable input stream
+
+The ChildProcess object also defines a pid property that specifies the process id of the
+child. And it defines a kill() method that you can use to terminate a child process
+
+
+16.10.4 fork()
+
+child_process.fork() is a specialized function for running a module of JavaScript
+code in a child Node process. fork() expects the same arguments as spawn(), but the
+first argument should specify the path to a file of JavaScript code instead of an executable
+binary file.
+
+A child process created with fork() can communicate with the parent process via its
+standard input and standard output streams, as described in the previous section for
+spawn(). But in addition, fork() enables another, much easier, communication channel
+between the parent and child processes.
+
+When you create a child process with fork(), you can use the send() method of the
+returned ChildProcess object to send a copy of an object to the child process. And
+you can listen for the “message” event on the ChildProcess to receive messages from
+the child. The code running in the child process can use process.send() to send a
+message to the parent and can listen for “message” events on process to receive messages
+from the parent.
+*/
+
+const child_process = require("child_process");
+
+// Start a new node process running the code in child.js in our directory
+let child = child_process.fork(`${__dirname}/child.js`);
+
+// Send a message to the child
+child.send({x: 4, y: 3});
+
+// Print the child's response when it arrives.
+child.on("message", message => {
+    console.log(message.hypotenuse); // This should print "5"
+    // Since we only send one message we only expect one response.
+    // After we receive it we call disconnect() to terminate the connection
+    // between parent and child. This allows both processes to exit cleanly.
+    child.disconnect();
+});
+
+//And here is the code that runs in the child process:
+
+// Wait for messages from our parent process
+process.on("message", message => {
+    // When we receive one, do a calculation and send the result
+    // back to the parent.
+    process.send({hypotenuse: Math.hypot(message.x, message.y)});
+});
+
+/*
+Starting child processes is an expensive operation, and the child process would have
+to be doing orders of magnitude more computation before it would make sense to
+use fork() and interprocess communication in this way. */
+
+/*If you are writing a program that needs to be very responsive to incoming events and 
+also needs to perform timeconsuming computations, then you might consider using a separate 
+child process to perform the computations so that they don’t block the event loop and reduce the
+responsiveness of the parent process.(Though a thread)*/
+
+
+//16.11 Worker Threads
 
