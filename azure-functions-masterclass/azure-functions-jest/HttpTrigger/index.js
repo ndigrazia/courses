24f27d9c348e3f222e619238aaae4cd16efc5dcc
Nelson <nelson.digrazia@gmail.com>
2023-08-07 16:23:40 -0300

    thirty second comments

---
+++ azure-functions-masterclass/azure-functions-jest/HttpTrigger/index.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if ((req.query.num1 && req.query.num2) && (!isNaN(req.query.num1) && !isNaN(req.query.num2))) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "The Result is: " + req.query.num1 * req.query.num2
+        }
+    } 
+    else { 
+        context.res = {
+            status: 400, /* Defaults to 200 */
+            body: "Params passed must be number!."
+        }
+    }  
+}
