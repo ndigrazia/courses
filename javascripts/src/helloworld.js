a172c6e5b3399994a1550636dbfd7563c829feb9
Nelson Di Grazia <nelson.digrazia@telefonica.com>
2023-09-07 17:13:24 -0300

    add comments

--- javascripts/src/helloworld.js
+++ javascripts/src/helloworld.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+console.log(isNaN(NaN)); // true
+console.log(isNaN(Number.NaN)); // true
+console.log(isNaN("t3")); // true
+
+console.log(Number.isNaN(NaN)); // true
+console.log(Number.isNaN(Number.NaN)); // true 
+
+console.log(Number.isNaN("t3")); // true
