a78dd0cfee271c0e3d3130c5c44709a7f8976122
Nelson Di Grazia <nelson.digrazia@telefonica.com>
2023-09-07 15:01:28 -0300

    remove all

--- azure-serverless-hands-on-learning/GetAllData_index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req, inputDocument) {
-	context.log('GetAllData function called.');
-	context.res = {
-		// status: 200, /* Defaults to 200 */
-		body: inputDocument
-	}
-};
